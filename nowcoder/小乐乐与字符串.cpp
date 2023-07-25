10ad8c8e84940776891e02ea405478e6ea3ab6b1
akalils <98333287+akalils@users.noreply.github.com>
2023-07-25 16:24:32 +0800

    study

---
+++ nowcoder/小乐乐与字符串.cpp
@@ -0,0 +1,24 @@
+////
+//// Created by 黎圣  on 2023/7/25.
+////
+//#include "iostream"
+//int main()
+//{
+//    char arr[8000] = {0};
+//    scanf("%s", arr);
+//    //统计子串CNH的个数
+//    long long c, ch, chn;
+//    char *p = arr;
+//    while (*p)
+//    {
+//        if (*p == 'C')
+//            c++;
+//        else if (*p == 'H')
+//            ch += c;
+//        else if (*p == 'N')
+//            chn += ch;
+//        p++;
+//    }
+//    printf("%lld\n", chn);
+//    return 0;
+//}
