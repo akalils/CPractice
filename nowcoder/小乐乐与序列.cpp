0dec377f9bed1845a76751ea8b8e806bc95b3217
akalils <98333287+akalils@users.noreply.github.com>
2023-07-25 16:24:57 +0800

    study

---
+++ nowcoder/小乐乐与序列.cpp
@@ -0,0 +1,24 @@
+////
+//// Created by 黎圣  on 2023/7/25.
+////
+//#include "stdio.h"
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int arr[100001] = {0};
+//    int temp;
+//    for (int i = 0; i < n; i++)
+//    {
+//        scanf("%d", &temp);
+//        arr[temp] = temp;
+//    }
+//    for (int i = 0; i < n; i++)
+//    {
+//        if (arr[i] != 0)
+//        {
+//            printf("%d ", arr[i]);
+//        }
+//    }
+//    return 0;
+//}
