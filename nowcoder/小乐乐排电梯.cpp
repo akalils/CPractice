e52a3335c9b15c62cf6e70b53cea892be06f72ab
akalils <98333287+akalils@users.noreply.github.com>
2023-07-25 16:31:35 +0800

    study

---
+++ nowcoder/小乐乐排电梯.cpp
@@ -0,0 +1,11 @@
+////
+//// Created by 黎圣  on 2023/7/25.
+////
+//#include <stdio.h>
+//
+//int main() {
+//    int n = 0;
+//    scanf("%d", &n);
+//    printf("%d", (n / 12) * 4 + 2);
+//    return 0;
+//}
