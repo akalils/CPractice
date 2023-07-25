ba314ccc8a4e5d2d8a403869f2b9eb5f6f3b18ef
akalils <98333287+akalils@users.noreply.github.com>
2023-07-25 16:25:25 +0800

    study

---
+++ nowcoder/小乐乐与欧几里德.cpp
@@ -0,0 +1,26 @@
+////
+//// Created by 黎圣  on 2023/7/25.
+////
+//#include <stdbool.h>
+//#include <stdio.h>
+//
+//int main() {
+//    long long n = 0;
+//    long long m = 0;
+//    scanf("%lld %lld", &n, &m);
+//    long long m2 = m;
+//    long long n2 = n;
+//    long long temp = 0;
+//    long long max = 0;
+//    long long min = 0;
+//    while (temp = n % m)
+//    {
+//        n = m;
+//        m = temp;
+//    }
+//    max = m;
+//    min = n2*m2/max;
+//    //最小公倍数=n*m/max
+//    printf("%lld", max + min);
+//    return 0;
+//}
