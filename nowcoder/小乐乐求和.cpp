6966da29e7d553f8291d2856c91016d32749c97d
akalils <98333287+akalils@users.noreply.github.com>
2023-07-25 16:30:30 +0800

    study

---
+++ nowcoder/小乐乐求和.cpp
@@ -0,0 +1,25 @@
+////
+//// Created by 黎圣  on 2023/7/25.
+////
+#include <stdio.h>
+//
+//int main() {
+//    int n = 0;
+//    long long sum = 0;
+//    scanf("%d", &n);
+//    for (int i = 1; i <= n; i++)
+//    {
+//        sum += i;
+//    }
+//    printf("%lld", sum);
+//    return 0;
+//}
+
+//int main() {
+//    long long n = 0;
+//    long long sum = 0;
+//    scanf("%lld", &n);
+//    sum = (1 + n) * n / 2;
+//    printf("%lld", sum);
+//    return 0;
+//}
