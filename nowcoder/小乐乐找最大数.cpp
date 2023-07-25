a89f0a485d4a61e5f214aac16425ee53f9c04808
akalils <98333287+akalils@users.noreply.github.com>
2023-07-25 16:34:43 +0800

    study

---
+++ nowcoder/小乐乐找最大数.cpp
@@ -0,0 +1,20 @@
+////
+//// Created by 黎圣  on 2023/7/25.
+////
+//#include <stdio.h>
+//
+//int main() {
+//    int arr[4] = {0};
+//    for (int i = 0; i < 4; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    int max = arr[0];
+//    for (int i = 1; i < 4; i++)
+//    {
+//        if (arr[i] > max)
+//            max = arr[i];
+//    }
+//    printf("%d\n", max);
+//    return 0;
+//}
