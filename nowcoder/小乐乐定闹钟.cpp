d605785f8097ad4c4f53c3f6ada6783d51241b23
akalils <98333287+akalils@users.noreply.github.com>
2023-07-25 16:26:12 +0800

    study

---
+++ nowcoder/小乐乐定闹钟.cpp
@@ -0,0 +1,13 @@
+////
+//// Created by 黎圣  on 2023/7/25.
+////
+//#include <stdio.h>
+//
+//int main() {
+//    int h, m, k;
+//    scanf("%d:%d %d", &h, &m, &k);
+//    h = ((m + k) / 60 + h) % 24;
+//    m = (m + k % 60) % 60;
+//    printf("%02d:%02d", h, m);
+//    return 0;
+//}
