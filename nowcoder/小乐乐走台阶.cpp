9140a851b667d48b34bb504c27829f5afea30e35
akalils <98333287+akalils@users.noreply.github.com>
2023-07-25 16:30:50 +0800

    study

---
+++ nowcoder/小乐乐走台阶.cpp
@@ -0,0 +1,19 @@
+////
+//// Created by 黎圣  on 2023/7/25.
+////
+//#include <stdio.h>
+//
+//int fib(int n)
+//{
+//    if (n <= 2)
+//        return n;
+//    else
+//        return fib(n-1) + fib(n - 2);
+//}
+//int main() {
+//    int n = 0;
+//    scanf("%d", &n);
+//    int ret = fib(n);
+//    printf("%d", ret);
+//    return 0;
+//}
