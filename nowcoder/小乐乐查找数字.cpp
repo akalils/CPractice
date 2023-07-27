518d532946d15d20f6c02a85a477eec7ff161e22
akalils <98333287+akalils@users.noreply.github.com>
2023-07-27 16:50:25 +0800

    Create 小乐乐查找数字.cpp

---
+++ nowcoder/小乐乐查找数字.cpp
@@ -0,0 +1,26 @@
+////
+//// Created by 黎圣  on 2023/7/27.
+////
+//#include <stdio.h>
+//
+//int main() {
+//    int n = 0;
+//    int arr[100] = {0};
+//    int count = 0;
+//    scanf("%d", &n);
+//    //n个数字的读取
+//    for (int i = 0; i < n; i++)
+//        scanf("%d", &arr[i]);
+//
+//    int x = 0;
+//    scanf("%d", &x);
+//    for (int i = 0; i < n; i++)
+//    {
+//        if (arr[i] == x)
+//        {
+//            count++;
+//        }
+//    }
+//    printf("%d", count);
+//    return 0;
+//}
