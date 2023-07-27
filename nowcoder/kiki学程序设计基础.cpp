e4c2b3bd8dad445bcfbbc55157dff3eb28e6bf78
akalils <98333287+akalils@users.noreply.github.com>
2023-07-27 16:51:24 +0800

    Create kiki学程序设计基础.cpp

---
+++ nowcoder/kiki学程序设计基础.cpp
@@ -0,0 +1,10 @@
+////
+//// Created by 黎圣  on 2023/7/27.
+////
+//#include "iostream"
+//int main()
+//{
+//    printf("printf(\"Hello world!\\n\");\n");
+//    printf("cout << \"Hello world!\" << endl;\n");
+//    return 0;
+//}
