8ae1e816d382b6dbede53b7318de66c87737aad9
akalils <98333287+akalils@users.noreply.github.com>
2023-10-17 19:22:28 +0800

    Create 2的幂.c

---
+++ Leetcode/2的幂.c
@@ -0,0 +1,12 @@
+bool isPowerOfTwo(int n){
+    if (n == 0)
+        return false;
+    if (n == 1073741824)
+        return true;
+    for (int i = 1; i < 1073741824; i *= 2)
+    {
+        if (n == i)
+            return true;
+    }
+    return false;
+}
