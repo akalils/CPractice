fe60586a11d89a21350e78ff1c7df5c4d918efe3
akalils <98333287+akalils@users.noreply.github.com>
2023-10-17 22:57:34 +0800

    Create 第N个泰波那契数列.c

---
+++ Leetcode/第N个泰波那契数列.c
@@ -0,0 +1,12 @@
+int f[40];
+
+int tribonacci(int n){
+    f[0] = 0;
+    f[1] = 1;
+    f[2] = 1;
+    for (int i = 3; i <= n; i++)
+    {
+        f[i] = f[i-1] + f[i-2] + f[i-3];
+    }
+    return f[n];
+}
