d88a1630017011d52ca38fa81a570447b6901edf
akalils <98333287+akalils@users.noreply.github.com>
2023-10-29 23:20:21 +0800

    Create 数组中两元素的最大乘积.c

---
+++ Leetcode/数组中两元素的最大乘积.c
@@ -0,0 +1,14 @@
+
+
+int maxProduct(int* nums, int numsSize){
+    int max = -1;
+    for (int i = 0; i < numsSize - 1; i++)
+    {
+        for (int j = i + 1; j < numsSize; j++)
+        {
+            if ((nums[i] - 1) * (nums[j] - 1) > max)
+                max = (nums[i] - 1) * (nums[j] - 1);
+        }
+    }
+    return max;
+}
